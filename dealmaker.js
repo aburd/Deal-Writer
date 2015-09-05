937f01076fc2dfecdfb279b8c8d73ddedd229071
Aaron Burdick <burdick.aaron@gmail.com>
2015-09-05 22:36:00 +0900

    Adding all the functionality in the javascript file to go along with the style changes.

--- dealmaker.js
+++ dealmaker.js
@@ -2,10 +2,11 @@ var month = 0;
 var monthEng = "";
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var voOld, voNew, comOld, comNew, addressOld, addressNew, meetingsOld, meetingsNew = ""; 
+var voOldb, voNewb, comOldb, comNewb, addressOldb, addressNewb, meetingsOldb, meetingsNewb = ""; 
 var floor1NameEn, floor1UrlEn, floor2NameEn, floor2UrlEn, area1 = "";
 var floor1NameJa, floor1UrlJa, floor2NameJa, floor2UrlJa, area2 = "";
         
-function makePromo() {
+function makeDealJa1() {
 
     var x = `<pre>
 &lt;div class="promo"&gt;
@@ -32,8 +33,36 @@ function makePromo() {
 
     return x;
 }
+
+function makeDealJa2() {
+
+    var x = `<pre>
+&lt;div class="promo"&gt;
+    &lt;div id="promo" style="width: 211px"&gt;&lt;strong&gt;${month}月限定オファー&lt;/strong&gt;
+        &lt;div id="promoin"&gt;                 
+            &lt;table&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;バーチャルオフィス&lt;br /&gt;&lt;span class="oldprice"&gt;&yen;${voOldb}&lt;/span&gt; &lt;strong&gt;&yen;${voNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;電話秘書代行 &lt;span class="oldprice"&gt;&yen;${comOldb}&lt;/span&gt; &lt;strong&gt;&yen;${comNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;貸し住所 &lt;span class="oldprice"&gt;&yen;${addressOldb}&lt;/span&gt; &lt;strong&gt;&yen;${addressNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;貸会議室 &lt;span class="oldprice"&gt;&yen;${meetingsOldb}&lt;/span&gt; &lt;strong&gt;&yen;${meetingsNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+            &lt;/table&gt;
+        &lt;/div&gt;
+    &lt;/div&gt;
+&lt;/div&gt; 
+    </pre>`;  
+
+    return x;
+}
         
-function makePromoEn(){
+function makeDealEn1(){
     var x = `<pre>
 &lt;div class="promo"&gt;
     &lt;div id="promo" style="width: 211px"&gt;&lt;strong&gt;${monthEng} Special!&lt;/strong&gt;
@@ -60,7 +89,34 @@ function makePromoEn(){
     return x;
 }
 
-function makeDealEn() {
+function makeDealEn2(){
+    var x = `<pre>
+&lt;div class="promo"&gt;
+    &lt;div id="promo" style="width: 211px"&gt;&lt;strong&gt;${monthEng} Special!&lt;/strong&gt;
+        &lt;div id="promoin"&gt;
+            &lt;table&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;Virtual Office &lt;span class="oldprice"&gt;&yen;${voOldb}&lt;/span&gt; &lt;strong&gt;&yen;${voNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;Communications &lt;span class="oldprice"&gt;&yen;${comOldb}&lt;/span&gt; &lt;strong&gt;&yen;${comNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;Address &lt;span class="oldprice"&gt;&yen;${addressOldb}&lt;/span&gt; &lt;strong&gt;&yen;${addressNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+                &lt;tr&gt;
+                    &lt;td&gt;Meetings &lt;span class="oldprice"&gt;&yen;${meetingsOldb}&lt;/span&gt; &lt;strong&gt;&yen;${meetingsNewb}&lt;/strong&gt;&lt;/td&gt;
+                &lt;/tr&gt;
+            &lt;/table&gt;             
+        &lt;/div&gt;
+    &lt;/div&gt;
+&lt;/div&gt;
+</pre>`;  
+
+    return x;
+}
+
+function makePromoEn() {
     var x = `<pre>
 &lt;div id="promo"&gt; 
     &lt;strong&gt; ${monthEng} Special!&lt;/strong&gt; 
@@ -73,7 +129,7 @@ function makeDealEn() {
     return x;
 }
 
-function makeDealJa() {
+function makePromoJa() {
     var x = `<pre>
 &lt;div class="sc-content" id="promo"&gt;&lt;strong&gt;${month}月限定スペシャル!&lt;/strong&gt;
     &lt;div class="sc-content" id="promoin"&gt;
@@ -104,6 +160,16 @@ function setVars() {
     addressNew = $("#price5").val();
     meetingsOld = $("#price6").val();
     meetingsNew = $("#price7").val();
+
+    voOldb = $("#priceb0").val();
+    voNewb = $("#priceb1").val();
+    comOldb = $("#priceb2").val();
+    comNewb = $("#priceb3").val();
+    addressOldb = $("#priceb4").val();
+    addressNewb = $("#priceb5").val();
+    meetingsOldb = $("#priceb6").val();
+    meetingsNewb = $("#priceb7").val();
+    
     monthEng = months[month-1];
     var $x = $('#floor1 option:selected');
     var $y = $('#floor2 option:selected');
@@ -119,22 +185,37 @@ function setVars() {
     floor2UrlJa = "http://www.servcorp.co.jp/ja" + $x.data('url');
 }
 
+function showNames(floor1, floor2, floor1en, floor2en){
+    $('#jaPage #floor1ja').html(floor1);
+    $('#jaPage #floor2ja').html(floor2);
+    $('#engPage #floor1en').html(floor1);
+    $('#engPage #floor2en').html(floor2);
+}
+
 $(document).ready( function(){    
    
     var jaPage = $('#bottom #jaPage'),
-        engPage = $('#bottom #engPage');
-    $('#enTab').click(function(){
+        engPage = $('#bottom #engPage'),
+        enTab = $('#enTab'),
+        jaTab = $('#jaTab');
+
+    enTab.click(function(){
         if(engPage.css('visibility', 'hidden')){
             jaPage.css('visibility', 'hidden');
-            engPage.css('visibility', 'visible'); 
+            jaTab.removeClass('activeTab');
+            engPage.css('visibility', 'visible');
+            enTab.addClass('activeTab'); 
         }
     });
-    $('#jaTab').click(function(){
+    jaTab.click(function(){
         
         if(jaPage.css('visibility', 'hidden')){
             engPage.css('visibility', 'hidden');
-            jaPage.css('visibility', 'visible'); 
+            enTab.removeClass('activeTab');
+            jaPage.css('visibility', 'visible');
+            jaTab.addClass('activeTab'); 
         }
+
     });
     
     $('select').click( function(){
@@ -183,12 +264,19 @@ $(document).ready( function(){
         if( compare ){
 
             $('#box').children('p').hide();
-            $('#output_jp').html(makePromo());
-            $('#deal_jp').html(makeDealJa());
-            $('#output_en').html(makePromoEn());
-            $('#deal_en').html(makeDealEn());
-            $('#box').children('p').fadeIn(2000);    
+
+            $('#output_jp').html(makePromoJa());
+            $('#deal_jp').html(makeDealJa1());
+            $('#deal_jpb').html(makeDealJa2());
             
+            $('#output_en').html(makePromoEn());
+            $('#deal_en').html(makeDealEn1());
+            $('#deal_enb').html(makeDealEn2());
+
+            showNames(floor1NameJa, floor2NameJa, floor1NameEn, floor2NameEn);
+
+            $('#jaPage').children('p').fadeIn(2000);    
+            $('#engPage').children('p').fadeIn(2000);    
             
             console.log(x);    
         } 
